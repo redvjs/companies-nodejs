d4df0aac36053bcf99eadfbc18370ba8b4b55c7a
redvjs <104998410+redvjs@users.noreply.github.com>
2025-02-10 19:13:49 +0000

    Add files via upload

---
+++ src/helperFunctions/toUpperCase.ts
@@ -0,0 +1,20 @@
+export function upperCaseWord(address: string) {
+    if (!address) return address;
+    try {
+        let [line1, town, county, postcode] = address.split("<br>");
+        let newAddress = "";
+
+        if (line1) line1 = line1.charAt(0).toUpperCase() + line1.slice(1);
+        if (town) town = town.charAt(0).toUpperCase() + town.slice(1);
+        if (county) county = county.charAt(0).toUpperCase() + county.slice(1);
+        if (postcode) postcode = postcode.toUpperCase();
+        if (!postcode.includes(" ")) {
+            postcode = postcode.slice(0,3) + " " + postcode.slice(3);
+        }
+
+        newAddress = [line1, town, county, postcode].filter(Boolean).join("<br>");
+        return newAddress;
+    } catch (err) {
+        return address;
+    }
+}
